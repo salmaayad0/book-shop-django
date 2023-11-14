aeab905cb3dbcde2b0b761ece72bf2e5ce090bc6
salmaayad0 <salmaayad017@gmail.com>
2023-11-14 14:50:11 +0200

    templates

---
+++ store/tests/test_models.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from store.models import Category, Product
+
+
+# Create your tests here.
+class CategoryTest(TestCase):
+    def setUp(self) -> None:
+        self.data = Category.objects.create(name='fun', slug='fun')
+    
+    def test_category(self):
+        testCategory = self.data
+        self.assertTrue(isinstance(testCategory, Category))
+        self.assertEqual(str(testCategory), 'fun')
+        
+             
+
+class ProductTest(TestCase): 
+    def setUp(self) -> None:
+        category = Category.objects.create(name='fun', slug='fun')
+        user = User.objects.create(username='admin')
+        self.data = Product.objects.create(title='booktest',slug='fun',
+                                           category=category,created_by=user, 
+                                           image='test.png',price='50.01')   
+        
+    def test_product(self):
+        testProduct = self.data
+        self.assertTrue(isinstance(testProduct, Product))
+        self.assertEqual(str(testProduct), 'booktest')
+        
+    
+
---
+++ store/tests/test_views.py
@@ -0,0 +1,54 @@
+from django.test import TestCase, Client, RequestFactory
+from django.urls import reverse
+from django.http import HttpRequest
+from django.contrib.auth.models import User
+from store.models import *
+from store.views import allBooks
+
+
+# Create your tests here.
+class TestViewResponse(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.factory = RequestFactory()
+        # as if these data created in database 'simulation'
+        category = Category.objects.create(name='test', slug='test')
+        user = User.objects.create(username='admin')
+        product = Product.objects.create(title='booktest',slug='booktest',
+                                         category=category,created_by=user, 
+                                           image='test.png',price='50.01') 
+    
+    
+    # test allowed urls => if data needed use from above as it's no works as db
+    def test_url_index(self):
+        response = self.c.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_absolute_category(self):
+        response = self.c.get(
+            reverse("store:listCategories", args=["test"]))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_url_absolute_product(self):
+        response = self.c.get(
+            reverse('store:getBook', args=['booktest']))
+        self.assertEqual(response.status_code, 200)
+        
+        
+    # test pages contains
+    def test_index_html(self):
+        request = HttpRequest()
+        response = allBooks(request)
+        htmlPage = response.content.decode('utf8')
+        self.assertIn('<title> Book Shop </title>', htmlPage)
+        self.assertEqual(response.status_code, 200)
+        
+        # another way to test view
+    def test_view_function(self):
+        request = self.factory.get('/book/booktest')
+        response = allBooks(request)
+        htmlPage = response.content.decode('utf8')
+        self.assertIn('<title> Book Shop </title>', htmlPage)
+        self.assertEqual(response.status_code, 200)
+        
+    
