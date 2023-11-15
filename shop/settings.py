4a855a3923417daf1efa8bc3d99d3468702cfd8b
salmaayad0 <salmaayad017@gmail.com>
2023-11-15 20:59:44 +0200

    edit templates

--- shop/settings.py
+++ shop/settings.py
@@ -126,7 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'shop/static')]
 
 # Media files (images are uploaded to)
 
